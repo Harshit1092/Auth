b6a0ea6e5398ba9072f63517ad62cb5a307277ef
Karanraj Mehta <karanrajmehta06@gmail.com>
2024-01-17 01:12:54 +0530

    minor bug fixes

--- client/src/components/CreateBlog.jsx
+++ client/src/components/CreateBlog.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from 'react';
 import { EditorState, convertToRaw, ContentState } from 'draft-js';
 import { Editor } from 'react-draft-wysiwyg';
@@ -13,128 +12,184 @@ import { toast, ToastContainer } from 'react-toastify';
 import { useAuth } from '../context/AuthContext';
 
 const CreateBlog = () => {
-    const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
-    const [content, setContent] = useState(EditorState.createEmpty());
-    const { currentUser } = useAuth();
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [content, setContent] = useState(EditorState.createEmpty());
+  const { currentUser } = useAuth();
+  const [isLoading, setIsLoading] = useState(false);
 
-    const handleTitleChange = (e) => {
-        setTitle(e.target.value);
-    };
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
 
-    const handleDescriptionChange = (e) => {
-        setDescription(e.target.value);
-    };
+  const handleDescriptionChange = (e) => {
+    setDescription(e.target.value);
+  };
 
-    const handleContentChange = (editorState) => {
-        setContent(editorState);
-    };
+  const handleContentChange = (editorState) => {
+    setContent(editorState);
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            console.log("CURRENT USER")
-            const htmlContent = stateToHTML(content.getCurrentContent());
-            // const htmlContent = convertToHTML(rawContentState);
-            // Handle form submission here
-            // send the blog data to the server
-            const blogData = {
-                title,
-                description,
-                content: htmlContent,
-                author: currentUser.name,
-                authorId: currentUser.id
-            };
-            
-            console.log(blogData);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setIsLoading(true);
+      console.log('CURRENT USER');
+      const htmlContent = stateToHTML(content.getCurrentContent());
+      // const htmlContent = convertToHTML(rawContentState);
+      // Handle form submission here
+      // send the blog data to the server
+      const blogData = {
+        title,
+        description,
+        content: htmlContent,
+        author: currentUser.name,
+        authorId: currentUser.id,
+      };
 
-            const response = await axios.post('http://localhost:8000/blog/createBlog',blogData)
+      console.log(blogData);
 
-            console.log("BLOGS CREATED SUCCESSFULLY", response);
-        }
-        catch(error){
-            console.log(error);
-            toast.error(error.response.data.error);
-        }
-        
+      const response = await axios.post(
+        'http://localhost:8000/blog/createBlog',
+        blogData
+      );
 
+      console.log('BLOGS CREATED SUCCESSFULLY', response);
+      toast.success('Blog Created Successfully');
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response.data.error);
+    } finally {
+      setIsLoading(false);
+    }
 
+    // console.log(content.getCurrentContent().getPlainText());
+  };
 
-        // console.log(content.getCurrentContent().getPlainText());
-    };
-
-    
-
-    return (
-        <>
-        <ToastContainer />
-        <Navbar />
-        <div className="container mx-auto p-4">
-            <h1 className="text-2xl font-bold mb-4 mt-2 text-indigo-600">Create Blog</h1>
-            <form onSubmit={handleSubmit}>
-                <div className="mb-4">
-                    <label htmlFor="title" className="block text-gray-700 font-bold mb-2">
-                        Title
-                    </label>
-                    <input
-                        type="text"
-                        id="title"
-                        className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-indigo-500"
-                        value={title}
-                        onChange={handleTitleChange}
-                    />
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="description" className="block text-gray-700 font-bold mb-2">
-                        Description
-                    </label>
-                    <textarea
-                        id="description"
-                        className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-indigo-500"
-                        value={description}
-                        onChange={handleDescriptionChange}
-                    ></textarea>
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="content" className="block text-gray-700 font-bold mb-2">
-                        Content
-                    </label>
-                    <Editor
-                        editorState={content}
-                        onEditorStateChange={handleContentChange}
-                        wrapperClassName="border border-gray-300 rounded focus:border-indigo-500"
-                        toolbar={{
-                            options: ['inline', 'blockType', 'fontSize', 'fontFamily', 'list', 'textAlign', 'colorPicker', 'link', 'embedded', 'emoji', 'image', 'remove', 'history'],
-                            inline: {
-                                options: ['bold', 'italic', 'underline', 'strikethrough', 'monospace', 'superscript', 'subscript'],
-                            },
-                            blockType: {
-                                options: ['Normal', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'Blockquote', 'Code'],
-                            },
-                            fontSize: {
-                                options: [8, 9, 10, 11, 12, 14, 16, 18, 24, 30, 36, 48, 60, 72, 96],
-                                className: 'bordered-option-classname',
-                                dropdownClassName: 'bordered-dropdown-classname',
-                            },
-                            fontFamily: {
-                                options: ['Arial', 'Georgia', 'Impact', 'Tahoma', 'Times New Roman', 'Verdana'],
-                                className: 'bordered-option-classname',
-                                dropdownClassName: 'bordered-dropdown-classname',
-                            },
-                        }}
-                    />
-                </div>
-                <button
-                    type="submit"
-                    className="bg-indigo-600 text-white py-2 px-4 rounded hover:bg-indigo-700"
-                >
-                    Post Blog
-                </button>
-            </form>
-        </div>
-        <Footer />
-        </>
-    );
+  return (
+    <>
+      <ToastContainer />
+      <Navbar />
+      <div className='container mx-auto p-4'>
+        <h1 className='text-2xl font-bold mb-4 mt-2 text-indigo-600'>
+          Create Blog
+        </h1>
+        <form onSubmit={handleSubmit}>
+          <div className='mb-4'>
+            <label
+              htmlFor='title'
+              className='block text-gray-700 font-bold mb-2'
+            >
+              Title
+            </label>
+            <input
+              type='text'
+              id='title'
+              className='w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-indigo-500'
+              value={title}
+              onChange={handleTitleChange}
+            />
+          </div>
+          <div className='mb-4'>
+            <label
+              htmlFor='description'
+              className='block text-gray-700 font-bold mb-2'
+            >
+              Description
+            </label>
+            <textarea
+              id='description'
+              className='w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-indigo-500'
+              value={description}
+              onChange={handleDescriptionChange}
+            ></textarea>
+          </div>
+          <div className='mb-4'>
+            <label
+              htmlFor='content'
+              className='block text-gray-700 font-bold mb-2'
+            >
+              Content
+            </label>
+            <Editor
+              editorState={content}
+              onEditorStateChange={handleContentChange}
+              wrapperClassName='border border-gray-300 rounded focus:border-indigo-500'
+              toolbar={{
+                options: [
+                  'inline',
+                  'blockType',
+                  'fontSize',
+                  'fontFamily',
+                  'list',
+                  'textAlign',
+                  'colorPicker',
+                  'link',
+                  'embedded',
+                  'emoji',
+                  'image',
+                  'remove',
+                  'history',
+                ],
+                inline: {
+                  options: [
+                    'bold',
+                    'italic',
+                    'underline',
+                    'strikethrough',
+                    'monospace',
+                    'superscript',
+                    'subscript',
+                  ],
+                },
+                blockType: {
+                  options: [
+                    'Normal',
+                    'H1',
+                    'H2',
+                    'H3',
+                    'H4',
+                    'H5',
+                    'H6',
+                    'Blockquote',
+                    'Code',
+                  ],
+                },
+                fontSize: {
+                  options: [
+                    8, 9, 10, 11, 12, 14, 16, 18, 24, 30, 36, 48, 60, 72, 96,
+                  ],
+                  className: 'bordered-option-classname',
+                  dropdownClassName: 'bordered-dropdown-classname',
+                },
+                fontFamily: {
+                  options: [
+                    'Arial',
+                    'Georgia',
+                    'Impact',
+                    'Tahoma',
+                    'Times New Roman',
+                    'Verdana',
+                  ],
+                  className: 'bordered-option-classname',
+                  dropdownClassName: 'bordered-dropdown-classname',
+                },
+              }}
+            />
+          </div>
+          <button
+            type='submit'
+            className={`text-white py-2 px-4 rounded hover:bg-indigo-700 ${
+              isLoading ? 'bg-indigo-400' : 'bg-indigo-700'
+            }`}
+          >
+            Post Blog
+          </button>
+        </form>
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default CreateBlog;
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,26 +1,24 @@
-const {Router}= require('express');
+const { Router } = require('express');
 const router = Router();
 const authController = require('../controllers/authController');
 const userController = require('../controllers/userController');
-router.get('/signup',authController.signup_get);
-router.post('/signup',authController.signup_post);
-router.get('/login',authController.login_get);
-router.post('/login',authController.login_post);
+router.get('/signup', authController.signup_get);
+router.post('/signup', authController.signup_post);
+router.get('/login', authController.login_get);
+router.post('/login', authController.login_post);
 
 // Remove 2 min waiting time
-router.post('/sendloginotp',authController.sendloginotp_post);
+router.post('/sendloginotp', authController.sendloginotp_post);
 
 // Remove 2 min waiting time
-router.post('/sendsignupotp',authController.sendsignupotp_post);
+router.post('/sendsignupotp', authController.sendsignupotp_post);
 
 // We'll remove this
-router.post('/resendotp',authController.resendotp_post);
-router.get('/logout',authController.logout_get);
+router.post('/resendotp', authController.resendotp_post);
+router.get('/logout', authController.logout_get);
 
-router.get('/get-profile',authController.getProfileInfo)
+router.get('/get-profile', authController.getProfileInfo);
 
-
-router.post('/updateuser',userController.updateuser_post);
-
+router.post('/update-profile', userController.updateuser_post);
 
 module.exports = router;
